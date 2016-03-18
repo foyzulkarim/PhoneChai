1c728ca470ca7b68810d4f7a581ed823c12b809f
Foyzul Karim <foyzulkarim@gmail.com>
2016-03-18 12:08:29 +0600

    added phonecontroller.cs

---
+++ Source/Phonechai/Phonechai.WebApp/Controllers/PhoneController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Phonechai.Model;
+using Phonechai.Service;
+
+namespace Phonechai.WebApp.Controllers
+{
+    public class PhoneController : BaseController
+    {
+        public IHttpActionResult Post(Phone phone)
+        {
+            PhoneService service = new PhoneService(Db);
+            if (string.IsNullOrWhiteSpace(phone.Id))
+            {
+                phone.Id = Guid.NewGuid().ToString();
+            }
+            // this is a test
+            string addedId =  service.Add(phone);
+            return Ok(addedId);
+        }
+
+    }
+}
