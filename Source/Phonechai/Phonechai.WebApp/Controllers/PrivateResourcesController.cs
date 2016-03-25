13fe4fc55a555ac007fd417c1c9bcde5bf0e3153
Foyzul Karim <foyzulkarim@gmail.com>
2016-03-25 11:56:24 +0600

    signup login ok from fiddler

---
+++ Source/Phonechai/Phonechai.WebApp/Controllers/PrivateResourcesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Phonechai.WebApp.Controllers
+{
+    [Authorize(Roles = "Admin",Users = "foyzulkarim@gmail.com")]
+    public class PrivateResourcesController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok(DateTime.Now);
+        }
+    }
+}
