71d15ce1446915fbaf410b280f1d8adca93538a4
Foyzul Karim <foyzulkarim@gmail.com>
2016-04-14 10:13:50 +0600

    added core requestmodel.ts

---
+++ Source/Phonechai/Phonechai.WebApp/App/Core/RequestModels.ts
@@ -0,0 +1,70 @@
+﻿module App {
+    "use strict";
+
+    export class Entity {
+        Id: string;
+        Created: string;
+        CreatedBy: string;
+        Modified: string;
+        ModifiedBy: string;
+    }
+     
+    export class PermissionRequest {
+        Name: string;
+
+        constructor(name: string) {
+            this.Name = name;
+        }
+    }
+
+    export class Notification {
+        IsError: boolean;
+        IsInfo: boolean;
+        Message: string;
+    }
+
+    export class DataRequest {
+        Id: string;
+        Page: number;
+        OrderBy: string;
+        Keyword: string;
+        IsAscending: string;
+        ParentId: string;
+        TotalPage: number;
+        StartDate: string;
+        EndDate: string;
+        Test: number;
+
+        protected getBaseQueryString(): string {
+            if (this.Page == null) this.Page = 1;
+            var queryString = `?keyword=${this.Keyword}&orderBy=${this.OrderBy}&page=${this.Page}`;
+            return queryString;
+        }
+    }
+
+    export class SearchRequest extends DataRequest {
+        constructor(keyword: string, orderBy: string, isAsc: string, parentId?: string) {
+            super();
+            this.Keyword = keyword;
+            this.OrderBy = orderBy;
+            this.IsAscending = isAsc;
+            this.ParentId = parentId;
+            this.Page = 1;
+        }
+    }
+
+    
+    export class DetailRequest extends DataRequest {
+        Id: string;
+
+        constructor(id: string) {
+            super();
+            this.Id = id;
+        }
+
+        GetQueryString(): string {
+            return `?id=${this.Id}`;
+        }
+    }
+    
+}
